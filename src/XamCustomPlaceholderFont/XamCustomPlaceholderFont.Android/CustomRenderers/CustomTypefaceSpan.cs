44df8e894e8173167db5d66bc8f4c185f60cd7e2
Aden Earnshaw <aden.earnshaw@webuyanycar.com>
2018-03-29 15:59:16 +0100

    Adding base project

---
+++ src/XamCustomPlaceholderFont/XamCustomPlaceholderFont.Android/CustomRenderers/CustomTypefaceSpan.cs
@@ -0,0 +1,36 @@
+﻿using Android.Graphics;
+using Android.Text;
+using Android.Text.Style;
+
+namespace XamCustomPlaceholderFont.Droid.CustomRenderers
+{
+    public class CustomTypefaceSpan : TypefaceSpan
+    {
+        private readonly Typeface _customTypeface;
+
+        public CustomTypefaceSpan(Typeface type) : base("")
+        {
+            _customTypeface = type;
+        }
+
+        public CustomTypefaceSpan(string family, Typeface type) : base(family)
+        {
+            _customTypeface = type;
+        }
+
+        public override void UpdateDrawState(TextPaint ds)
+        {
+            ApplyCustomTypeFace(ds, _customTypeface);
+        }
+
+        public override void UpdateMeasureState(TextPaint paint)
+        {
+            ApplyCustomTypeFace(paint, _customTypeface);
+        }
+
+        private static void ApplyCustomTypeFace(Paint paint, Typeface tf)
+        {
+            paint.SetTypeface(tf);
+        }
+    }
+}
---
+++ src/XamCustomPlaceholderFont/XamCustomPlaceholderFont/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using XamCustomPlaceholderFont.Views;
+
+namespace XamCustomPlaceholderFont
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+    }
+}
