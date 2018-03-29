bf6c27b916d24028e9c8bf98350ec9240e14d18b
Aden Earnshaw <aden.earnshaw@webuyanycar.com>
2018-03-29 15:59:16 +0100

    Adding base project

---
+++ src/XamCustomPlaceholderFont/XamCustomPlaceholderFont.Android/CustomRenderers/CustomPlaceholderEntryRenderer.cs
@@ -0,0 +1,55 @@
+﻿using Android.Content;
+using Android.Text;
+using Android.Text.Style;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamCustomPlaceholderFont.Controls;
+using XamCustomPlaceholderFont.Droid.CustomRenderers;
+
+[assembly: ExportRenderer(typeof(CustomPlaceholderEntry), typeof(CustomPlaceholderEntryRenderer))]
+namespace XamCustomPlaceholderFont.Droid.CustomRenderers
+{
+    public class CustomPlaceholderEntryRenderer : EntryRenderer
+    {
+        private CustomPlaceholderEntry CustomElement => Element as CustomPlaceholderEntry;
+
+        public CustomPlaceholderEntryRenderer(Context context) : base(context)
+        { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            UpdatePlaceholderFont();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == CustomPlaceholderEntry.PlaceholderFontProperty.PropertyName)
+                UpdatePlaceholderFont();
+        }
+
+        private void UpdatePlaceholderFont()
+        {
+            if (CustomElement.PlaceholderFont == Font.Default)
+            {
+                Control.HintFormatted = null;
+                Control.Hint = CustomElement.Placeholder;
+                Control.SetHintTextColor(CustomElement.PlaceholderColor.ToAndroid());
+                return;
+            }
+
+            var placeholderFontSize = 30; //TODO: Work out correct height
+            var placeholderSpan = new SpannableString(CustomElement.Placeholder);
+            placeholderSpan.SetSpan(new AbsoluteSizeSpan(placeholderFontSize, false), 0, placeholderSpan.Length(), SpanTypes.InclusiveExclusive); // Set Fontsize
+
+            var typeFaceSpan = new CustomTypefaceSpan(CustomElement.PlaceholderFont.ToTypeface());
+            placeholderSpan.SetSpan(typeFaceSpan, 0, placeholderSpan.Length(), SpanTypes.InclusiveExclusive); //Set Fontface
+
+            Control.HintFormatted = placeholderSpan;
+        }
+    }
+}
---
+++ src/XamCustomPlaceholderFont/XamCustomPlaceholderFont/Controls/CustomPlaceholderEntry.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace XamCustomPlaceholderFont.Controls
+{
+    public class CustomPlaceholderEntry : Entry
+    {
+        public static readonly BindableProperty PlaceholderFontProperty
+            = BindableProperty.Create(nameof(PlaceholderFont), typeof(Font), typeof(CustomPlaceholderEntry), default(Font));
+
+        public Font PlaceholderFont
+        {
+            get { return (Font)GetValue(PlaceholderFontProperty); }
+            set { SetValue(PlaceholderFontProperty, value); }
+        }
+    }
+}
